e5bbebb840dbc27e095c0bda8a69e0751152c0c9
Lucas van der Kleij <kleijvander@gmail.com>
2026-08-16 15:40:56 +0200

    set up vnet for backend - ai communicaation

---
+++ infrastructure/networking.tf
@@ -0,0 +1,40 @@
+resource "azurerm_virtual_network" "vnet_portfolio_prod" {
+  name                = "vnet-portfolio-prod"
+  location            = azurerm_resource_group.rg_portfolio_prod.location
+  resource_group_name = azurerm_resource_group.rg_portfolio_prod.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "snet_container_apps_prod" {
+  name                 = "snet-container-apps-prod"
+  resource_group_name  = azurerm_resource_group.rg_portfolio_prod.name
+  virtual_network_name = azurerm_virtual_network.vnet_portfolio_prod.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  service_endpoints = [
+    "Microsoft.CognitiveServices"
+  ]
+
+  delegation {
+    name = "container-apps"
+
+    service_delegation {
+      name = "Microsoft.App/environments"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "nsg_container_apps_prod" {
+  name                = "nsg-container-apps-prod"
+  location            = azurerm_resource_group.rg_portfolio_prod.location
+  resource_group_name = azurerm_resource_group.rg_portfolio_prod.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "container_apps" {
+  subnet_id                 = azurerm_subnet.snet_container_apps_prod.id
+  network_security_group_id = azurerm_network_security_group.nsg_container_apps_prod.id
+}
