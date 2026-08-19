3283e1c40085f4634ff732ed0412422afec964aa
Lucas van der Kleij <kleijvander@gmail.com>
2026-08-19 19:52:35 +0200

    tf format

--- infrastructure/apps.tf
+++ infrastructure/apps.tf
@@ -73,7 +73,7 @@ resource "azurerm_container_app" "ca_portfolio_backend_prod" {
 }
 
 resource "azurerm_role_assignment" "backend_foundry_agent_consumer" {
-  scope = azurerm_cognitive_account.aif_portfolio_prod.id
+  scope                = azurerm_cognitive_account.aif_portfolio_prod.id
   role_definition_name = "Cognitive Services User"
   principal_id         = azurerm_container_app.ca_portfolio_backend_prod.identity[0].principal_id
 }
