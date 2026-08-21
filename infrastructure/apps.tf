953d2d742921765e77a684b8401ad40b18d058d8
Lucas van der Kleij <kleijvander@gmail.com>
2026-08-21 13:25:13 +0200

    configure cloudflare zone settings

--- infrastructure/apps.tf
+++ infrastructure/apps.tf
@@ -254,3 +254,27 @@ resource "cloudflare_ruleset" "rate_limit" {
     }
   }]
 }
+
+resource "cloudflare_zone_setting" "tls_1_3" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "tls_1_3"
+  value      = "on"
+}
+
+resource "cloudflare_zone_setting" "automatic_https_rewrites" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "automatic_https_rewrites"
+  value      = "on"
+}
+
+resource "cloudflare_zone_setting" "ssl" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "ssl"
+  value      = "strict"
+}
+
+resource "cloudflare_zone_setting" "always_use_https" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "always_use_https"
+  value      = "on"
+}
