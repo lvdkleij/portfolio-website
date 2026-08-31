3af87d63ad1ae866efd6410a8076f56543563c78
Lucas van der Kleij <kleijvander@gmail.com>
2026-08-31 14:39:06 +0200

    fix prompt

--- backend/modules/infrastructure/chatclient/src/main/java/eu/roodbaard/aichat/infrastructure/chatclient/config/LucasAssistantChatClientConfig.java
+++ backend/modules/infrastructure/chatclient/src/main/java/eu/roodbaard/aichat/infrastructure/chatclient/config/LucasAssistantChatClientConfig.java
@@ -2,11 +2,16 @@ package eu.roodbaard.aichat.infrastructure.chatclient.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class LucasAssistantChatClientConfig {
+
+    @Value("classpath:promptTemplates/systemPromptTemplate.st")
+    Resource systemPromptTemplate;
 
     @Bean
     public ChatClient lucasAssistantChatClient(OpenAiChatModel openAiChatModel) {
